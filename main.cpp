61c1afabfb29400857d9112cf00e95c4f0f8bf57
luciana1faur <luciana_faur@yahoo.com>
2020-09-26 17:36:20 +0000

    Paranteze la formula /3

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main() {
   cout << "Cat este radius? ";
   cin >> radius;
 
-  volum = 4 * pi * radius * radius * radius / 3;
+  volum = (4 * pi * radius * radius * radius) / 3;
 
   cout << "Volumul unei sfere cu radian " << radius << " este " << volum << endl;
 
